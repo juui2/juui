4a714e4420ce18af7709c54c8fdad020cdf6911b
rubenabix <rubenabix@egmail.com>
2017-01-24 14:47:50 -0600

    test layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'juui!';
+  public direction = "row";
+  toggleDirection() {
+    this.direction = (this.direction === "column") ? "row" : "column";
+  }
 }
